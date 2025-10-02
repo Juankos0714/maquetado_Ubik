e650e5529fb7dcc5627aca5f853f4930abc815e0
Julian <juliango2494@gmail.com>
2025-10-02 13:39:16 -0500

    feature-explora v2  agregado a header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Profile } from "./views/profile/profile";
 import { Home } from './views/home/home';
 import { Destinos } from './views/destinos/destinos';
+import { Explora } from './views/explora/explora';
 
 export const routes: Routes = [
     {path: '',component:Home},
     {path: 'profile', component: Profile},
-    {path: 'destinations', component: Destinos} // redirect to home for unknown paths
+    {path: 'destinations', component: Destinos}, // redirect to home for unknown paths
+    {path: 'explora', component: Explora}
 ];
