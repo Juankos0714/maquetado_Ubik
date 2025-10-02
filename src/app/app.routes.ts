439e65603b4261eeffd86e4bf0bee0bfd17ce08e
Miguel-A-VN <mvargasnavarro19@gmail.com>
2025-10-02 13:42:57 -0500

    Creando vista offers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Profile } from "./views/profile/profile";
 import { Home } from './views/home/home';
 import { Destinos } from './views/destinos/destinos';
+import { Offers } from './views/offers/offers';
 
 export const routes: Routes = [
     {path: '',component:Home},
     {path: 'profile', component: Profile},
-    {path: 'destinations', component: Destinos} // redirect to home for unknown paths
+    {path: 'destinations', component: Destinos},
+    {path: 'offers', component: Offers},
 ];
