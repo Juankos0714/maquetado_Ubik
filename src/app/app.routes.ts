1bf5999e063c1691d7a3d9dbb9aa6b3dc69f4c73
Miguel-A-VN <mvargasnavarro19@gmail.com>
2025-10-02 10:07:02 -0500

    Elimine los olt-router redundantes y el header y footer de componets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,7 @@
 import { Routes } from '@angular/router';
-import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer';
 import { Home } from './views/home/home';
 
 
 export const routes: Routes = [
-    {path: '',component:Home},
-    {path:'header',component:Header},
-    {path:'footer',component:Footer}
-
+    {path: '',component:Home}
 ];
--- src/app/views/home/home.ts
+++ src/app/views/home/home.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { Header } from '../../components/header/header';
-import { Footer } from '../../components/footer/footer';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [Header,Footer,RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
